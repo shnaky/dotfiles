98716742f26c1f1eb984f000550baad3bed1ae49
Orestis Papandreou <orestis.papandreou@gmail.com>
2023-02-22 12:04:07 +0100

    cmp with Tab

--- nvim/lua/ori/plugins-setup.lua
+++ nvim/lua/ori/plugins-setup.lua
@@ -90,7 +90,7 @@ return require("packer").startup(function(use)
 
 	-- auto closing
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags for html
 
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
