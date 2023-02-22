ca6eb4ec81db8a90491610d50dd6356c48431d67
Orestis Papandreou <orestis.papandreou@gmail.com>
2023-02-22 12:57:43 +0100

    tree floating window

--- nvim/lua/ori/plugins/tree.lua
+++ nvim/lua/ori/plugins/tree.lua
@@ -1,36 +1,28 @@
 -- import settings file with protected call
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
-
 
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+-- vim.g.nvim_tree_auto_close = true
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
--- require("nvim-tree").setup()
-nvimtree.setup()
-
--- OR setup with some options
--- require("nvim-tree").setup({
---   sort_by = "case_sensitive",
---   view = {
---     width = 30,
---     mappings = {
---       list = {
---         { key = "u", action = "dir_up" },
---       },
---     },
---   },
---   renderer = {
---     group_empty = true,
---   },
---   filters = {
---     dotfiles = true,
---   },
--- })
+-- tree config setup
+nvimtree.setup({
+	-- windows / buffer setup
+	view = {
+		-- floating window
+		float = {
+			enable = true,
+			quit_on_focus_loss = true, -- close tree when out of focus
+		},
+	},
+	renderer = {
+		group_empty = true, -- empty folders shown as one
+	},
+})
